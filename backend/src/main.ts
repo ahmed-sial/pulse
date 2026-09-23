94aa4f90822710f2106c92b7ad0a2600fbb2039e
Ahmed Hassan <0ahmed.sial@gmail.com>
2026-09-23 20:54:42 +0500

    refactor(backend): add global prefix and verisoning in api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module.js';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
   app.enableCors({
     origin: '*',
     credentials: true,
